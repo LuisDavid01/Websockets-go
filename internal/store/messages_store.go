dc799473d513bc61d9fd065c1d295323566df1ef
Luis David <luisdavidmirandav12@gmail.com>
2025-06-22 12:08:59 -0600

    something

--- internal/store/messages_store.go
+++ internal/store/messages_store.go
@@ -20,7 +20,7 @@ type Messages struct {
 
 type IMessages interface {
 	ReadChat(rID, pID int64) (*Chat, error)
-	SendMessage(rID, PID int64) error
+	SendMessage(rID, PID int64, msg []byte) error
 	BlockChat(chatID int64) error
 	TransferChat(rID, chatID int64) error
 	DownloadChat(chatID int64) ([]byte, error)
@@ -32,4 +32,40 @@ type DbConn struct {
 
 func NewDbConn(db *sql.DB) *DbConn {
 	return &DbConn{db: db}
+}
+
+func (pg *DbConn) ReadChat(rID, pID int64) (*Chat, error) {
+	chatMessages := &Chat{}
+	query := `
+	SELECT * from messages
+	WHERE $1 = recept_id AND
+	$2 = pacient_id
+	`
+	result, err := pg.db.Query(query, rID, pID)
+
+	if err == sql.ErrNoRows {
+		return nil, err
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	for result.Next() {
+		var msg Messages
+		err = result.Scan(
+			&msg.ID,
+			&msg.Message,
+			&msg.PacientID,
+			&msg.ReceptionistID,
+			&msg.CreatedAt,
+			msg.IsRead,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		chatMessages.content = append(chatMessages.content, msg)
+	}
+	return chatMessages, nil
+
 }
